6249dd141766decdc6ec294e9388c57497f577fe
Moses Journey <journey@buck.tv>
2015-03-06 18:39:48 -0800

    added basic layer comp name changer

---
+++ layerCompRenamer_prototype.jsx
@@ -0,0 +1,17 @@
+﻿{
+    // 2014, use it at your own risk;
+
+    #target photoshop
+
+    if (app.documents.length > 0) {
+
+        var myDocument = app.activeDocument;
+        var selectedComps = new Array();
+
+        for (var m = 0; m < myDocument.layerComps.length; m++) {
+            if (myDocument.layerComps[m].selected){
+                myDocument.layerComps[m].name = "susanIsAWESOME_"+m.toString();
+            }
+        }
+    }
+}
